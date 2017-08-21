0d5900daf06aa02d1bb59c9927f6b9a7bd3b34ed
bart@BN <bart@bamweb.nl>
2017-08-21 11:15:30 +0200

    install_profile.sh - removed git clones

--- install_profile.sh
+++ install_profile.sh
@@ -94,24 +94,6 @@ if [[ ! $(id -u) == 0 ]]; then
 		rm $HOME/.root_aliases
 	fi
 	ln -sf $PROFILEDIR/$SUB/root_aliases $HOME/.root_aliases 
-	### Openhab-vim
-	if [[ ! -d $HOME/git/openhab-vim ]] ; then
-		git clone git@github.com:bambam82/openhab-vim.git $HOME/git/openhab-vim
-	fi
-	if [[ -e $HOME/.vim/syntax/openhab.vim ]]; then
-		rm $HOME/.vim/syntax/openhab.vim
-	fi
-	ln -sf $HOME/git/openhab-vim/syntax/openhab.vim $HOME/.vim/syntax/openhab.vim
-	if [[ -e $HOME/.vim/ftdetect/openhab.vim ]]; then
-		rm $HOME/.vim/ftdetect/openhab.vim
-	fi
-	ln -sf $HOME/git/openhab-vim/ftdetect/openhab.vim $HOME/.vim/ftdetect/openhab.vim
-
-	echo "Downloading other directories"
-	echo "	Network scripts"
-	if [[ ! -d $HOME/git/network_scripts ]] ; then
-		git clone git@github.com:bambam82/network_scripts.git $HOME/git/network_scripts
-	fi
 
 	### Python environment
 	if [[ ! -e $HOME/.pypirc ]] ; then 
