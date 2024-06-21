a03c9f6c84238315f1e124d4aa6346390836c07b
Bart Dorlandt <bart@bamweb.nl>
2024-06-21 15:09:44 +0200

    pyenv fix

--- fish_user/conf.d/pyenv.fish
+++ fish_user/conf.d/pyenv.fish
@@ -1,4 +1,4 @@
-if test -e /usr/local/bin/pyenv || test -e $HOME/.pyenv/bin/pyenv || test -e /opt/homebrew/bin/pyenv
+if test -e /usr/local/bin/pyenv; or test -e $HOME/.pyenv/bin/pyenv; or test -e /opt/homebrew/bin/pyenv;
     # this is done once. (Universal variables)
     pyenv init - | source
 end
