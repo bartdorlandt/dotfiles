7720c229dc47bbf90bd621776d01fbfc06d94ea9
Bart Dorlandt <bart@bamweb.nl>
2024-06-21 15:17:41 +0200

    venvcreate fix

--- fish_user/functions/venvcreate.fish
+++ fish_user/functions/venvcreate.fish
@@ -1,11 +1,11 @@
 function venvcreate
     set -f VERSION $argv[1]
     if not set -q VERSION[1]
-        set -f VERSION 3
+        set -f VERSION ""
     end
 
 	set -l COMMAND (string join "" "python" $VERSION)
 	_CMD $COMMAND -m venv .venv $argv[2..]
 	_CMD source ./.venv/bin/activate.fish
-	_CMD python3 -m ensurepip --upgrade
+	_CMD python -m pip install -U pip
 end
