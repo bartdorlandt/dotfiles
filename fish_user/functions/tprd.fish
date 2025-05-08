1d9e58dd2a04866d1fa55afce3c9d82781a27529
Bart Dorlandt <bart@bamweb.nl>
2025-05-08 16:11:34 +0200

    modified tsh scripts

--- fish_user/functions/tprd.fish
+++ fish_user/functions/tprd.fish
@@ -1,4 +1,10 @@
 function tprd
-	tsh goprd && tsh ssh (fzf < $HOME/.teleport_hosts_prd)
+    tsh goprd
 
+    tsh ls --format names >$HOME/.teleport_hosts
+    cp -a $HOME/.teleport_hosts $HOME/.teleport_hosts_prd
+
+    set -l host (fzf < $HOME/.teleport_hosts_prd)
+    echo "Connecting to $host"
+    tsh ssh $host
 end
--- fish_user/functions/tstg.fish
+++ fish_user/functions/tstg.fish
@@ -1,3 +1,10 @@
 function tstg
-	tsh gostg && tsh ssh (fzf < $HOME/.teleport_hosts_stg)
+    tsh gostg
+
+    tsh ls --format names >$HOME/.teleport_hosts
+    cp -a $HOME/.teleport_hosts $HOME/.teleport_hosts_stg
+
+    set -l host (fzf < $HOME/.teleport_hosts_stg)
+    echo "Connecting to $host"
+    tsh ssh $host
 end
